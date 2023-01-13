d827c5f5a4e64657ee424e318d31184c99769131
Jonathan <jonato96s@gmail.com>
2023-01-12 23:00:18 -0500

    Global loading completed

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import GlobalLoading from "../common/GlobalLoading";
 
 const MainLayout = () => {
 
   return (
-    <>  
+    <>          
+    <GlobalLoading/>
 
       {/* Todo login modal */}      
       {/* login modal */}

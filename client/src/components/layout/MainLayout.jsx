0d2fecf54d6b96becd5b448774befbbc0476ef46
Javier Vallejo <rjavier001@gmail.com>
2023-01-13 09:46:41 -0500

    circular rate added

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -27,6 +27,19 @@ const MainLayout = () => {
     authUser();
   }, [dispatch]);
 
+  useEffect(() => {
+    const getFavorites = async () => {
+      const { response, err } = await favoriteApi.getList();
+
+      if (response) dispatch(setListFavorites(response));
+      if (err) toast.error(err.message);
+    };
+
+    if (user) getFavorites();
+    if (!user) dispatch(setListFavorites([]));
+  }, [user, dispatch]);
+
+
   return (
     <>          
     <GlobalLoading/>

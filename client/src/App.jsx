bba9d157aa5188a1537ccc9675e38f240047bef2
Jonathan <jonato96s@gmail.com>
2023-01-12 21:35:46 -0500

    Theme configs completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
+import { ThemeProvider } from "@emotion/react";
+import { useSelector } from "react-redux";
+import themeConfigs from "./configs/theme.configs";
+import {ToastContainer} from "react-toastify"
+import  CssBaseline  from "@mui/material/CssBaseline";
+
 const App = () => {
+  const {themeMode} = useSelector((state)=>state.themeMode)
   return (
-    <div>
-      App
-    </div>
+    <ThemeProvider theme={themeConfigs.custom({mode: themeMode})}>
+      
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+      
+      <CssBaseline />
+
+    </ThemeProvider>
   );
 }
 

882ddbf2e103abf9a5ee630aab6c05ff49627371
Bryan-ITSQMET <91750960+Bryan-ITSQMET@users.noreply.github.com>
2023-01-14 00:01:03 -0500

    edit request.handler.js

--- server/src/handlers/request.handler.js
+++ server/src/handlers/request.handler.js
@@ -3,9 +3,10 @@ import { validationResult } from "express-validator";
 const validate = (req, res, next) => {
   const errors = validationResult(req);
 
-  if (!errors.isEmpty()) return res.status(400).json({
-    message: errors.array()[0].msg
-  });
+  if (!errors.isEmpty())
+    return res.status(400).json({
+      message: errors.array()[0].msg,
+    });
 
   next();
 };

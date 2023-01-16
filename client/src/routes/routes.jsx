8c18a06f7bad9c816bfba7fcc5d211256e7c3c75
Bryan-ITSQMET <91750960+Bryan-ITSQMET@users.noreply.github.com>
2023-01-15 19:30:49 -0500

    add-function_form

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -7,7 +7,7 @@ import MediaSearch from "../pages/MediaSearch";
 import PasswordUpdate from "../pages/PasswordUpdate";
 import ReviewList from "../pages/ReviewList";
 import ProtectedPage from "../components/common/ProtectedPage";
- import Form from "../pages/Form";
+import Form from "../pages/Form";
 
 export const routesGen = {
   home: "/",
